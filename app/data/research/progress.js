8fb052b7751ea0a878968bd5ef1284850f0af0e0
Jiayi <bjyella@gmail.com>
2019-04-08 23:18:45 -0400

    Updates

--- app/data/research/progress.js
+++ app/data/research/progress.js
@@ -9,11 +9,6 @@ const progress = [
     author: '',
     degree: 'Overshooting in Startup Fundraising: A Doomed Bubble or a Great Lie?'
   },
-  {
-    school: 'Currently in Writing',
-    author: '(with Alex Rees-Jones and Mark Shepard), ',
-    degree: 'Dual-Process Mental Accounting and the Consumption/Savings Behavior of Retirees'
-  },
 ];
 
 export default progress;
