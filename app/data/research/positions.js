082d7e5f844ecddb4dd1bc5c16c241b832ddc589
Jiayi <bjyella@gmail.com>
2022-07-06 11:56:24 -0400

    “Updates”

--- app/data/research/positions.js
+++ app/data/research/positions.js
@@ -1,6 +1,12 @@
 
 const positions = [
 {
+    school: '1st Revise and Resubmit at Strategic Management Journal',
+    author: 'Bao, Jiayi',
+    degree: 'Healthcare Social Safety Nets, Gender, and Innovative Entrepreneurship.',
+    abstract: 'This paper examines whether improved healthcare social safety nets reduce the gender gap in innovative entrepreneurship. I argue that improved healthcare social safety nets provide substitutes to employer-provided health insurance, thus removing job mobility constraints by lowering the opportunity cost of leaving employment. I propose that women are affected more than men and that innovative women are particularly more likely to enter entrepreneurship than noninnovative women because of a stronger preference for health-related benefits and a greater propensity to pursue entrepreneurial opportunities. Leveraging the quasi-experiment of the Affordable Care Act implementation, I find consistent evidence supporting these arguments and a reduced gender gap in science, technology, engineering, and mathematics (STEM) entrepreneurship. The findings shed light on how institutional policies promote diversity in innovative entrepreneurship.'
+  },
+  {
     school: 'Under Review at Strategic Management Journal',
     author: 'Bao, Jiayi',
     degree: 'Do Makerspaces Affect Entrepreneurship ? Evidence on Entry, Experimentation, and Commercialization.',
@@ -11,12 +17,6 @@ const positions = [
     author: 'Bao, Jiayi',
     degree: 'Does Unlimited Vacation Benefit Firms? An Examination of How and When.',
     abstract: 'This paper provides a comprehensive study of unlimited vacation, an employee benefit that entails schedule flexibility and autonomy, through three complementary settings. Qualitative data (Setting 1) on benefits reviews are explored to motivate theoretical development. Evidence from a randomized controlled trial (Setting 2) suggests that, despite more vacation use, the unlimited vacation scheme improves overall labor efficiency through two channels that increase labor productivity: (i) attracting more high-performers and (ii) improving individual productivity. A field study (Setting 3) confirms the productivity effect and increased vacation use. Together, four organizational conditions in performance systems and social relationships are found to support larger labor efficiency gains under unlimited vacation: a stronger firing threat for under- performance, bundled features that emphasize performance, better managerial support, and greater team cohesion.'
-  },
-  {
-    school: 'Under Review at Strategic Management Journal',
-    author: 'Bao, Jiayi',
-    degree: 'Healthcare Social Safety Nets, Gender, and Innovative Entrepreneurship.',
-    abstract: 'This paper examines whether improved healthcare social safety nets reduce the gender gap in innovative entrepreneurship. I argue that improved healthcare social safety nets provide substitutes to employer-provided health insurance, thus removing job mobility constraints by lowering the opportunity cost of leaving employment. I propose that women are affected more than men and that innovative women are particularly more likely to enter entrepreneurship than noninnovative women because of a stronger preference for health-related benefits and a greater propensity to pursue entrepreneurial opportunities. Leveraging the quasi-experiment of the Affordable Care Act implementation, I find consistent evidence supporting these arguments and a reduced gender gap in science, technology, engineering, and mathematics (STEM) entrepreneurship. The findings shed light on how institutional policies promote diversity in innovative entrepreneurship.'
   },
    {
     school: 'Under Review at Organization Science',
